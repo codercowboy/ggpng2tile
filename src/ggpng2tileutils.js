a5587af1a7dc5e47882207f572ed2b0c1a4893ed
Jason Baker <jason@onejasonforsale.com>
2026-06-24 13:01:25 -0700

    Fix rename issue step 1 - remove files

--- src/ggpng2tileutils.js
+++
@@ -1,8 +0,0 @@
-export function denull(value, defaultValue) {
-  return value == null ? defaultValue : value;
-}
-
-export function cloneObject(object) {
-  let clone = object == null ? null : Object.assign(Object.create(Object.getPrototypeOf(object)), object);
-  return clone;
-}
